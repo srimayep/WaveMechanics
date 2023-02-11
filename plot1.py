615d3a60505d77cf4a2ef8ccfa702429b491814a
srimayep <76884682+srimayep@users.noreply.github.com>
2023-02-11 15:20:43 -0500

    Create plot1.py

---
+++ plot1.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(-10, 10, 1000)
+y = 1/np.sqrt(10) * 1/np.cosh(x/5)
+
+plt.plot(x, y)
+plt.xlabel('x')
+plt.ylabel(r'$\psi(x)$')
+plt.title(r'Plot of $\psi(x) = \frac{1}{\sqrt{10}} \cdot \text{sech}(x/5)$')
+plt.show()
